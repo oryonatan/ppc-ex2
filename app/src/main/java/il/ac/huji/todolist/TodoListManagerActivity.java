b6d0694a2e22341de40685b0f66e3f2279ee6603
Yonatan Oren <or.yonatan@gmail.com>
2015-03-12 20:23:31 +0200

    sadsa

--- app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
+++ app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -1,13 +1,20 @@
 package il.ac.huji.todolist;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -23,8 +30,35 @@ public class TodoListManagerActivity extends ActionBarActivity {
         ListView lstTodo = (ListView)findViewById(R.id.lstTodoItems);
         todoAdapdter = new RedBlueAdapter(this,R.layout.red_blue_list,itemsInList);
         lstTodo.setAdapter(todoAdapdter);
+
+        lstTodo.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                showDeleteDialog(parent, view, position, id);
+                return false;
+            };
+        });
     }
 
+    public void showDeleteDialog(AdapterView<?> parent, View view, final int position, long id) {
+
+        Activity activity = (Activity)parent.getContext();
+        final Dialog inviteBuilder = new Dialog(activity);
+        LayoutInflater inflater = (LayoutInflater)
+                activity.getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View bodyView = inflater.inflate(R.layout.delete_dialog,null);
+        ((TextView)bodyView.findViewById(R.id.menuItemDeleteTitle)).setText(itemsInList.get(position));
+        bodyView.findViewById(R.id.menuItemDelete).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemsInList.remove(position);
+                todoAdapdter.notifyDataSetChanged();
+                inviteBuilder.hide();
+            }
+        });
+        inviteBuilder.setContentView(bodyView);
+        inviteBuilder.show();
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
